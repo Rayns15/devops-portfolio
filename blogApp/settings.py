498152d41d44821f0668e51b1ebcefaaeba2923b
Emanuel Panait <panaitemanuel@gmail.com>
2024-05-18 08:34:20 +0300

    Added PostgreSQL database

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'blogApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',                      
-#         'USER': 'postgres',
-#         'PASSWORD': 'cqGiSNnstwaz2lyPFb4N',
-#         'HOST': 'containers-us-west-69.railway.app',
-#         'PORT': '6440',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',                      
+        'USER': 'rayns',
+        'PASSWORD': '.Installuser01',
+        'HOST': 'devportfoliodb.c14u4kewsypk.eu-central-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
