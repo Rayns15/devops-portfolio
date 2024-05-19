2e22470d7ad50a766f53ecdc4d46686bb954d6e3
Emanuel Panait <panaitemanuel@gmail.com>
2024-05-19 22:48:24 +0300

    Updated the PostgreSQL DB

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -92,23 +92,23 @@ WSGI_APPLICATION = 'blogApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',                      
-#         'USER': 'rayns',
-#         'PASSWORD': '.Installuser01',
-#         'HOST': 'devportfoliodb.c14u4kewsypk.eu-central-1.rds.amazonaws.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',                      
+        'USER': 'rayns',
+        'PASSWORD': '.Installuser01',
+        'HOST': 'devportfoliodb.c14u4kewsypk.eu-central-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
